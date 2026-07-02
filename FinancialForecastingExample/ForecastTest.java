b563c6973370963019b60b318b95c7f987590ff4
SAJNA12345 <sajnasarfudeen@gmail.com>
2026-07-02 20:24:05 +0530

    Added Cognizant Deep Skilling exercises

---
+++ FinancialForecastingExample/ForecastTest.java
@@ -0,0 +1,18 @@
+package FinancialForecastingExample;
+
+public class ForecastTest {
+
+    public static void main(String[] args) {
+
+        double presentValue = 10000;     // Initial investment
+        double growthRate = 0.10;        // 10% annual growth
+        int years = 5;
+
+        double future = FinancialForecast.futureValue(presentValue, growthRate, years);
+
+        System.out.printf("Present Value : %.2f%n", presentValue);
+        System.out.printf("Growth Rate   : %.0f%%%n", growthRate * 100);
+        System.out.println("Years         : " + years);
+        System.out.printf("Future Value  : %.2f%n", future);
+    }
+}
