7e9997c51d42fc142aa6df7b60db55d73fc916f7
SAJNA12345 <sajnasarfudeen@gmail.com>
2026-07-03 20:13:27 +0530

    Added Setup JUnit exercise

---
+++ SetUpJunit/src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+
+        int result = 5 + 3;
+
+        assertEquals(8, result);
+
+    }
+}
