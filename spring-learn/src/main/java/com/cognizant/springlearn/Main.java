8f58106d78cb60d229353fd85d4a37fbd578d5b6
SAJNA12345 <sajnasarfudeen@gmail.com>
2026-07-13 19:51:40 +0530

    next

---
+++ spring-learn/src/main/java/com/cognizant/springlearn/Main.java
@@ -0,0 +1,10 @@
+package com.cognizant.springlearn;
+
+import static com.cognizant.springlearn.SpringLearnApplication.displayCountry;
+
+public class Main {
+
+    public static void main(String[] args) {
+        displayCountry();
+    }
+}
--- spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,22 +1,28 @@
-
-
 package com.cognizant.springlearn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class SpringLearnApplication {
-
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(SpringLearnApplication.class);
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringLearnApplication.class, args);
+        System.out.println("Main Started");
 
-        LOGGER.info("Inside main");
+        displayCountry();
+
+        System.out.println("Main Ended");
+    }
+
+    public static void displayCountry() {
+
+        System.out.println("Inside displayCountry()");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        System.out.println(country);
     }
 }
