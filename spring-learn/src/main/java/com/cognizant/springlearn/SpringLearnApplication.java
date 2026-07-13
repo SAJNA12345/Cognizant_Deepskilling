3cbe496def8ba1222ef23ab95627c1ba07fdb44c
SAJNA12345 <sajnasarfudeen@gmail.com>
2026-07-13 20:05:14 +0530

    next

--- spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,28 +1,40 @@
+//package com.cognizant.springlearn;
+//
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//public class SpringLearnApplication {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println("Main Started");
+//
+//        displayCountry();
+//
+//        System.out.println("Main Ended");
+//    }
+//
+//    public static void displayCountry() {
+//
+//        System.out.println("Inside displayCountry()");
+//
+//        ApplicationContext context =
+//                new ClassPathXmlApplicationContext("country.xml");
+//
+//        Country country = context.getBean("country", Country.class);
+//
+//        System.out.println(country);
+//    }
+//}
 package com.cognizant.springlearn;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringLearnApplication {
 
     public static void main(String[] args) {
-
-        System.out.println("Main Started");
-
-        displayCountry();
-
-        System.out.println("Main Ended");
-    }
-
-    public static void displayCountry() {
-
-        System.out.println("Inside displayCountry()");
-
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country country = context.getBean("country", Country.class);
-
-        System.out.println(country);
+        SpringApplication.run(SpringLearnApplication.class, args);
     }
 }
